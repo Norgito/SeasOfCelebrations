3b2883e4603240e772dc55939c20eb9520f2a32a
Norgito <tito.cubanito@gmail.com>
2023-05-15 13:04:37 -0300

    Chunk test

--- src/components/BalloonsHeader.jsx
+++ src/components/BalloonsHeader.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import Balloon from "../assets/Bcart.png";
+// import Balloon from "../assets/Bcart.png";
 
 const BalloonsHeader = ({ page }) => {
   let text = "";
@@ -15,11 +15,11 @@ const BalloonsHeader = ({ page }) => {
   return (
     <div>
       <div className="text-[40px] font-secondary text-center Center flex-col react-floater-animated mb-10">
-        <img src={Balloon} className="w-[120px] lg:mr-[345px] absolute" />
+        {/* <img src={Balloon} className="w-[120px] lg:mr-[345px] absolute" />
         <img
           src={Balloon}
           className="w-[120px] lg:ml-[345px] absolute hidden lg:block"
-        />
+        /> */}
         <div className="z-20 Center">
           <p className="text-white AnimatedBg2 shadow-lg rounded-full md:w-[400px] w-[350px] mt-[160px] h-auto">
             {text}
