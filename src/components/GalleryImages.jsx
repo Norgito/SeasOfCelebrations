c09d5340168388d4b4513bc26fe78eb7ede3fd55
Norgito <tito.cubanito@gmail.com>
2023-05-15 12:29:20 -0300

    Gallery Fix

--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -1,36 +1,15 @@
+import { useState, useEffect } from "react";
 import "../styles/Gallery.css";
 import FsLightbox from "fslightbox-react";
-import { useState, useEffect } from "react";
-import Img1 from "../assets/img/Gallery/Seas01.jpg";
-import Img2 from "../assets/img/Gallery/Seas02.jpg";
-import Img3 from "../assets/img/Gallery/Seas03.jpg";
-import Img5 from "../assets/img/Gallery/Seas05.jpg";
-import Img6 from "../assets/img/Gallery/Seas06.jpg";
-import Img7 from "../assets/img/Gallery/Seas07.jpg";
-import Img8 from "../assets/img/Gallery/Seas08.jpg";
-import Img9 from "../assets/img/Gallery/Seas09.jpg";
-import Img10 from "../assets/img/Gallery/Seas10.jpg";
-import Img11 from "../assets/img/Gallery/Seas11.jpg";
-import Img12 from "../assets/img/Gallery/Seas12.jpg";
-import Img13 from "../assets/img/Gallery/Seas13.jpg";
 
 const GalleryImages = () => {
   const [toggler, setToggler] = useState(false);
   const [sourceIndex, setSourceIndex] = useState(1);
 
   const images = [
-    Img1,
-    Img2,
-    Img3,
-    Img5,
-    Img6,
-    Img7,
-    Img8,
-    Img9,
-    Img10,
-    Img11,
-    Img12,
-    Img13,
+    "https://i.imgur.com/syyH46l.jpg",
+    "https://i.imgur.com/syyH46l.jpg",
+    "https://i.imgur.com/syyH46l.jpg",
   ];
 
   useEffect(() => {
@@ -61,7 +40,7 @@ const GalleryImages = () => {
                 }}
                 className="img rounded-xl"
                 style={{
-                  backgroundImage: `url(${image.replace("/thumbs/", "/")})`,
+                  backgroundImage: `url(${image})`,
                 }}
               />
             </a>
